e76eb073364e708f5c4b7e047181a9934bd25288
jesus <jesuscalles2001@gmail.com>
2025-02-27 12:50:00 -0400

    nueva estructura de rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useRoutes } from "react-router-dom";
-import Themeroutes from "./routes/Router";
+
+import FullLayout from "./layouts/FullLayout";
 
 const App = () => {
-  const routing = useRoutes(Themeroutes);
 
-  return <div className="dark">{routing}</div>;
+
+  return <div className="dark">
+    <FullLayout/>
+  </div>;
 };
 
 export default App;
--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -1,25 +1,63 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useRoutes, Navigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { Container } from "reactstrap";
 
+/***** Componentes de Rutas *****/
+import Starter from "../views/Starter";
+import About from "../views/About";
+import Alerts from "../views/ui/Alerts";
+import Badges from "../views/ui/Badges";
+import Buttons from "../views/ui/Buttons";
+import Cards from "../views/ui/Cards";
+import Grid from "../views/ui/Grid";
+import Forms from "../views/ui/Forms";
+import Breadcrumbs from "../views/ui/Breadcrumbs";
+import Login from "../components/login/login";
+import ProjectTables from "../components/dashboard/ProjectTable";
+import TablaDirectivaLocal from "../components/directivaLocal/tablaDirectivaLocal";
+import TablaRegistrarIglesia from "../components/iglesiasxzona/registrarIglesia";
+import TablaDirectivaZonal from "../components/directivaZonal/tablaDirectivaZonal";
+
 const FullLayout = () => {
-  const location = useLocation(); // Hook para obtener la ubicación actual
-  const isLoginPage = location.pathname; // Verifica si estás en la página de login
-console.log(location.pathname)
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login"; // Verifica si estás en la página de login
+
+  // Definición de rutas usando useRoutes
+  const routes = useRoutes([
+    { path: "/", element: <Navigate to="/login" replace /> },
+    { path: "/starter", element: <Starter /> },
+    { path: "/about", element: <About /> },
+    { path: "/alerts", element: <Alerts /> },
+    { path: "/badges", element: <Badges /> },
+    { path: "/buttons", element: <Buttons /> },
+    { path: "/cards", element: <Cards /> },
+    { path: "/grid", element: <Grid /> },
+    { path: "/registrojoyas", element: <ProjectTables /> },
+    { path: "/directivajslocal", element: <TablaDirectivaLocal /> },
+    { path: "/directivajszonal", element: <TablaDirectivaZonal /> },
+    { path: "/registrariglesia", element: <TablaRegistrarIglesia /> },
+    { path: "/forms", element: <Forms /> },
+    { path: "/breadcrumbs", element: <Breadcrumbs /> },
+    { path: "/login", element: <Login /> },
+  ]);
+
   return (
     <main>
       <div className="pageWrapper d-lg-flex">
-        {/********Sidebar**********/}
-        {isLoginPage.toString() !== '/login' ? ( // Condicional para ocultar el Sidebar si estás en /login
+        {/******** Sidebar **********/}
+        {!isLoginPage && (
           <aside className="sidebarArea shadow" id="sidebarArea">
             <Sidebar />
           </aside>
-        ) : '' }
-        {/********Content Area**********/}
-        <div className="contentArea">
-          {/********Middle Content**********/}
+        )}
+        
+        {/******** Content Area **********/}
+        <div
+          className="w-100 p-4 m-0 py-0 mx-auto"
+          style={{ height: "85dvh", overflow: "auto" }}
+        >
           <Container className="p-4" fluid>
-            <Outlet />
+            {routes}
           </Container>
         </div>
       </div>
